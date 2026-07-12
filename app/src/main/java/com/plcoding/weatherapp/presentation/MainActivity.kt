5a54b26f8ef3eb9899a8ed7b2333360aa490322c
Trafi25 <a345lexj45@gmail.com>
2026-07-12 21:32:00 +0200

    feat(build): fix build files

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContent {
-//            WeatherAppTheme {
-//
-//            }
-//        }
+        setContent {
+            WeatherAppTheme {
+
+            }
+        }
     }
 }
