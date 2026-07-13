23fc3b9e2468e02e524d5745c001f944d059c011
Trafi25 <a345lexj45@gmail.com>
2026-07-13 21:05:23 +0200

    add(presentation): add presentation main layer

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
@@ -4,5 +4,6 @@ import com.plcoding.weatherapp.domain.weather.WeatherInfo
 
 data class WeatherState(
     val weatherInfo: WeatherInfo? = null,
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val error: String? = null
 )
--- app/src/main/java/com/plcoding/weatherapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/ui/theme/Color.kt
@@ -2,5 +2,5 @@ package com.plcoding.weatherapp.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkBlue = Color(0xFF1B3B5A)
-val DeepBlue = Color(0xFF102840)
+val DarkGreen = Color(0xFF12372A)
+val DeepGreenBackground = Color(0xFF081C15)
