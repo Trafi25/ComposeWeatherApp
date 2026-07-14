abbe3d003f51f989607d0461bd8c214eb2a8b2bc
Trafi25 <a345lexj45@gmail.com>
2026-07-14 19:30:21 +0200

    fix(presentation): improve state handling

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
@@ -1,9 +1,13 @@
 package com.plcoding.weatherapp.presentation
 
+import androidx.compose.runtime.Immutable
 import com.plcoding.weatherapp.domain.weather.WeatherInfo
 
+@Immutable
 data class WeatherState(
     val weatherInfo: WeatherInfo? = null,
+    val locationName: String? = null,
     val isLoading: Boolean = false,
-    val error: String? = null,
+    val errorMessage: String? = null,
+    val hasLocationPermission: Boolean = false,
 )
