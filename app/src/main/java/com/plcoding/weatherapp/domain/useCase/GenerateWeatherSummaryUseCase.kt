44efbaba8dfc2cde2821902183c0c279f96928b3
Trafi25 <a345lexj45@gmail.com>
2026-08-09 14:57:12 +0200

    ci: add google-services.json generation to Android CI workflow
    - Add a workflow step to create `app/google-services.json` from the `GOOGLE_SERVICES_JSON` secret to support Google Services during the CI process

--- app/src/main/java/com/plcoding/weatherapp/domain/useCase/GenerateWeatherSummaryUseCase.kt
+++ app/src/main/java/com/plcoding/weatherapp/domain/useCase/GenerateWeatherSummaryUseCase.kt
@@ -4,18 +4,17 @@ import com.plcoding.weatherapp.domain.repository.WeatherAiRepository
 import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import javax.inject.Inject
 
-class GenerateWeatherSummaryUseCase @Inject constructor(
-    private val repository: WeatherAiRepository,
-){
-
-    suspend operator fun invoke(
-        weatherInfo: WeatherInfo,
-        locationName: String,
-    ) : String {
-        return repository.generateWeatherSummary(
-            weatherInfo = weatherInfo,
-            locationName = locationName
-        )
+class GenerateWeatherSummaryUseCase
+    @Inject
+    constructor(
+        private val repository: WeatherAiRepository,
+    ) {
+        suspend operator fun invoke(
+            weatherInfo: WeatherInfo,
+            locationName: String,
+        ): String =
+            repository.generateWeatherSummary(
+                weatherInfo = weatherInfo,
+                locationName = locationName,
+            )
     }
-
-}
